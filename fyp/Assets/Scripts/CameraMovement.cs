bfedaff2d81eb3b31aed5feae0c4d7bceafe676c
LawCheukHung <80800524+LawCheukHung@users.noreply.github.com>
2021-10-18 12:07:18 +0800

    updates

---
+++ fyp/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Camera playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCam.transform.eulerAngles = new Vector3();
+    }
+}
