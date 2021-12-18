115aadfe2ede00c5e10fc96a01156ed66ac74dab
LawCheukHung <80800524+LawCheukHung@users.noreply.github.com>
2021-12-18 19:07:32 +0800

    updates player behavior, adds hand and teacher book objects

--- fyp/Assets/Scripts/NPC/StudentBehavior.cs
+++ fyp/Assets/Scripts/NPC/StudentBehavior.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class StudentBehavior : MonoBehaviour
 {
-    public Rigidbody studentRigi;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        studentRigi = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
     void Update()
     {
---
+++ fyp/Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject teachingBook;
+    private TeacherState teacherState;
+
+    void Start()
+    {
+        teacherState = TeacherState.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switchMode();
+
+        if(teacherState == TeacherState.Catch)
+        {
+            catchStudent();
+        }
+    }
+
+    private void switchMode()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            teachMode();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            catchMode();
+        }
+    }
+
+    private void teachMode()
+    {
+        if (teacherState == TeacherState.Teach)
+        {
+            idleMode();
+        }
+        else
+        {
+            idleMode();
+            teachingBook.SetActive(true);
+            teacherState = TeacherState.Teach;
+        }
+    }
+
+    private void catchMode()
+    {
+        if (teacherState == TeacherState.Catch)
+        {
+            idleMode();
+        }
+        else
+        {
+            idleMode();
+            teacherState = TeacherState.Catch;
+        }
+    }
+
+    private void idleMode()
+    {
+        teachingBook.SetActive(false);
+        teacherState = TeacherState.Idle;
+    }
+
+    private void catchStudent()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("fuck");
+        }
+    }
+}
